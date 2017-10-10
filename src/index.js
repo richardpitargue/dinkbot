317ff106094c0e41c087692df36d7f6d8fb85d1b
richardpitargue <jlpitargue@up.edu.ph>
2017-10-10 19:36:08 +0800

    Move bot token to config file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Discord from 'discord.js';
+import config from '../config';
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('i\'m ready!');
 });
+
+client.login(config.TOKEN);
