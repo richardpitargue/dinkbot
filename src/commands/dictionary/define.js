38ce2ca2ab1659f0a1b91bbed9f1787874300f09
richardpitargue <jlpitargue@up.edu.ph>
2017-10-11 15:41:12 +0800

    Implement dictionary define

---
+++ src/commands/dictionary/define.js
@@ -0,0 +1,49 @@
+import rp from 'request-promise';
+import { RichEmbed } from 'discord.js';
+import config from '../../../config';
+
+export default function(word, message) {
+    const options = generate_options(word);
+
+    if(!options) {
+        message.channel.send('There was an error processing your request. Try again.')
+    }
+
+    rp(options)
+    .then((result) => {
+        const dictionaryEntry = result.results[0];
+        const lexicalEntries = dictionaryEntry.lexicalEntries;
+
+        const embed = new RichEmbed();
+
+        embed.setTitle('Definitions for the word: ' + dictionaryEntry.word);
+        embed.setColor(0x0070FF);
+        for(let i = 0; i < lexicalEntries.length; i++) {
+            let fieldValue = '';
+            for(let j = 0; j < Math.min(lexicalEntries[i].entries[0].senses.length, 3); j++) {
+                fieldValue = fieldValue + '- ' + lexicalEntries[i].entries[0].senses[j].definitions[0] + '\n';
+            }
+            embed.addField(lexicalEntries[i].lexicalCategory, fieldValue);
+        }
+
+        message.channel.send({embed});
+    })
+    .catch((error) => {
+        console.log(error);
+        message.channel.send('There was an error processing your request. Try again.')
+    });
+}
+
+function generate_options(word) {
+    if(!word || word === '') return false;
+
+    return {
+        uri: 'https://od-api.oxforddictionaries.com/api/v1/entries/en/' + word.toLowerCase(),
+        headers: {
+            'User-Agent': 'Request-Promise',
+            'app_id': config.DICTIONARY_API.APP_ID,
+            'app_key': config.DICTIONARY_API.APP_KEY
+        },
+        json: true
+    };
+}
--- src/commands/handler.js
+++ src/commands/handler.js
@@ -1,13 +1,22 @@
 import config from '../../config';
 
+import define from './dictionary/define';
+
 export default function(message) {
     if(message.author.bot) return; // ignore messages sent by bots
     if(!message.content.startsWith(config.PREFIX)) return; //ignore messages that does not begin with the prefix
-    
-    const args = message.content.trim().split('/ +/g');
+
+    const args = message.content.trim().split(' ');
     const command = args.shift().slice(config.PREFIX.length);
 
     if(command === 'ping') {
         message.channel.send('pong!');
+    } else
+    if(command === 'define'){
+        if(args.length > 1) {
+            message.channel.send('I will only provide definitions for the first word you provided, which is ' + args[0]);
+        }
+
+        define(args[0], message);
     }
 }
