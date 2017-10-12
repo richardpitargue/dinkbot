efa38b84dd1daebaa73fdb7cbfef33b3f7b0add4
richardpitargue <jlpitargue@up.edu.ph>
2017-10-12 15:33:36 +0800

    Modify define command to use the API library

--- src/commands/dictionary/define.js
+++ src/commands/dictionary/define.js
@@ -1,52 +1,23 @@
-import rp from 'request-promise';
+import dictionary from '../../lib/dictionary';
 import { RichEmbed } from 'discord.js';
-import config from '../../../config';
 
 export default function(word, message) {
-    const options = generate_options(word);
-
-    if(!options) {
-        message.channel.send('There was an error processing your request. Try again.')
-    }
-
-    rp(options)
-    .then((result) => {
-        const dictionaryEntry = result.results[0];
-        const lexicalEntries = dictionaryEntry.lexicalEntries;
-
-        const embed = new RichEmbed();
-
-        embed.setTitle('Definitions for the word: ' + dictionaryEntry.word);
-        embed.setColor(0x0070FF);
-        for(let i = 0; i < lexicalEntries.length; i++) {
-            let fieldValue = '';
-            for(let j = 0; j < Math.min(lexicalEntries[i].entries[0].senses.length, 3); j++) {
-                fieldValue = fieldValue + '- ' + lexicalEntries[i].entries[0].senses[j].definitions[0] + '\n';
-            }
-            embed.addField(lexicalEntries[i].lexicalCategory, fieldValue);
-        }
-
-        message.channel.send({embed});
-    })
-    .catch((error) => {
-        if(error.statusCode === 404) {
-            message.channel.send('I could not find definitions for the word: ' + word + '. Please try again.');
+    const embed = new RichEmbed();
+    dictionary.define(word, (result) => {
+        if(result.type === 'error') {
+            embed.setDescription('Could not find a definition for the word ' + word + '. Check your spelling?');
+            embed.setColor(0xFF4136);
+            message.channel.send({embed});
             return;
         }
-        message.channel.send('There was an error processing your request. Try again.')
+
+        const res = result.result;
+
+        embed.setTitle('Definitions for the word **' + res.word + '**');
+        embed.setColor(0x0074D9);
+        for(let i = 0; i < res.entries.length; i++) {
+            embed.addField(res.entries[i].lexicalCategory, '- ' + res.entries[i].definitions.join('\n- '));
+        }
+        message.channel.send({embed});
     });
-}
-
-function generate_options(word) {
-    if(!word || word === '') return false;
-
-    return {
-        uri: 'https://od-api.oxforddictionaries.com/api/v1/entries/en/' + word.toLowerCase(),
-        headers: {
-            'User-Agent': 'Request-Promise',
-            'app_id': config.DICTIONARY_API.APP_ID,
-            'app_key': config.DICTIONARY_API.APP_KEY
-        },
-        json: true
-    };
 }
