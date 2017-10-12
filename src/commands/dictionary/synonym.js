bc779bacb60d6057831f56a71341eb9ec84edef2
richardpitargue <jlpitargue@up.edu.ph>
2017-10-12 15:34:00 +0800

    Implement synonym command

---
+++ src/commands/dictionary/synonym.js
@@ -0,0 +1,23 @@
+import dictionary from '../../lib/dictionary';
+import { RichEmbed } from 'discord.js';
+
+export default function(word, message) {
+    const embed = new RichEmbed();
+    dictionary.synonyms(word, (result) => {
+        if(result.type === 'error') {
+            console.log(result.message);
+            console.log(result.error);
+            embed.setDescription('Could not find synonyms for the word ' + word + '. Check your spelling?');
+            embed.setColor(0xFF4136);
+            message.channel.send({embed});
+            return;
+        }
+
+        const res = result.result;
+
+        embed.setTitle('Synonyms for the word **' + res.word + '**');
+        embed.setColor(0x0074D9);
+        embed.setDescription(res.entries.join(', '));
+        message.channel.send({embed});
+    });
+}
