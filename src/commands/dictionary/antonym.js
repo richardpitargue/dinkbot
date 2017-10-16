f38968505fae5af413c9d7f4c6fba64ab292c5df
richardpitargue <jlpitargue@up.edu.ph>
2017-10-16 20:40:51 +0800

    Change the way the results are sent to the chat

--- src/commands/dictionary/antonym.js
+++ src/commands/dictionary/antonym.js
@@ -2,6 +2,7 @@ import dictionary from '../../lib/dictionary';
 import { RichEmbed } from 'discord.js';
 
 export default async function(word, message) {
+    const m = await message.channel.send('Searching antonyms of the word ' + word.toLowerCase() + '...');
     try {
         const embed = new RichEmbed();
         const result = await dictionary.antonyms(word);
@@ -9,10 +10,11 @@ export default async function(word, message) {
         embed.setTitle('Antonyms for the word **' + result.word + '**');
         embed.setColor(0x0074D9);
         embed.setDescription(result.entries.join(', '));
-        message.channel.send({embed});
+        embed.setFooter('Requested by: ' + message.author.username, message.author.avatarURL);
+        m.edit({embed});
     } catch(e) {
         if(e.statusCode === 404) {
-            message.channel.send('better check your spelling, ho, i dont know that shit.');
+            m.edit('better check your spelling, ho, i dont know that shit.');
         }
     }
 }
--- src/commands/dictionary/define.js
+++ src/commands/dictionary/define.js
@@ -2,6 +2,7 @@ import dictionary from '../../lib/dictionary';
 import { RichEmbed } from 'discord.js';
 
 export default async function(word, message) {
+    const m = await message.channel.send('Searching definitions of the word ' + word.toLowerCase() + '...');
     try {
         const embed = new RichEmbed();
         const result = await dictionary.define(word);
@@ -12,10 +13,11 @@ export default async function(word, message) {
         for(let i = 0; i < result.entries.length; i++) {
             embed.addField(result.entries[i].lexicalCategory, '- ' + result.entries[i].definitions.join('\n- '));
         }
-        message.channel.send({embed});
+        embed.setFooter('Requested by: ' + message.author.username, message.author.avatarURL);
+        m.edit({embed});
     } catch(e) {
         if(e.statusCode === 404) {
-            message.channel.send('Bitch ass ho that word doesn\'t exist fuck off.');
+            m.edit('Bitch ass ho that word doesn\'t exist fuck off.');
         }
     }
 }
