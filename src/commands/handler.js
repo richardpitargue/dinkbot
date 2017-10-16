9f077be3a1c18db47d2b50b541ea61c21902bc1f
richardpitargue <jlpitargue@up.edu.ph>
2017-10-16 20:39:21 +0800

    Add new command: urban

--- src/commands/handler.js
+++ src/commands/handler.js
@@ -3,6 +3,7 @@ import config from '../../config';
 import define from './dictionary/define';
 import synonym from './dictionary/synonym';
 import antonym from './dictionary/antonym';
+import urban from './dictionary/urban';
 
 export default function(message) {
     if(message.author.bot) return; // ignore messages sent by bots
@@ -34,5 +35,12 @@ export default function(message) {
         }
 
         antonym(args[0], message);
+    } else
+    if(command === 'urban') {
+        if(args.length > 1) {
+            message.channel.send('I will only provide definitions for the first word you provided, which is ' + args[0]);
+        }
+
+        urban(args[0], message);
     }
 }
