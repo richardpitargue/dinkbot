3092233bde65aef725edcd4bd2d591926b7ccacb
richardpitargue <jlpitargue@up.edu.ph>
2017-11-04 17:21:25 +0800

    Use gulp instead of npm scripts for tasks

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp   = require('gulp');
+const rollup = require('rollup');
+const eslint = require('rollup-plugin-eslint');
+const spawn  = require('child_process').spawn;
+
+let node;
+
+gulp.task('build', async () => {
+    const bundle = await rollup.rollup({
+        input: './src/index.js',
+        plugins: [
+            eslint({
+                fix: true
+            })
+        ]
+    });
+
+    await bundle.write({
+        file: './build/bundle.js',
+        format: 'cjs',
+        sourcemap: true
+    });
+});
+
+gulp.task('start', ['build'], () => {
+    if(node) node.kill();
+    node = spawn('node', ['build/bundle.js'], {stdio: 'inherit'})
+    node.on('close', function (code) {
+        if (code === 8) {
+            gulp.log('Error detected, waiting for changes...');
+        }
+    });
+});
+
+gulp.task('default', ['start'], () => {
+    gulp.watch(['./src/**/*.js'], ['start']);
+});
